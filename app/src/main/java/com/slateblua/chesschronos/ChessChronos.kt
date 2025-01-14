f4d42105a60ee65cd4aa6814798fba01743e3837
Sergey Hayriyan <sega1ness@gmail.com>
2025-01-15 00:38:26 +0400

    updated app launch entry

--- app/src/main/java/com/slateblua/chesschronos/ChessChronos.kt
+++ app/src/main/java/com/slateblua/chesschronos/ChessChronos.kt
@@ -6,13 +6,16 @@ import androidx.activity.compose.setContent
 import cafe.adriel.voyager.navigator.Navigator
 import cafe.adriel.voyager.transitions.SlideTransition
 import com.slateblua.chesschronos.feature.newgame.NewGameScreen
+import org.koin.compose.KoinContext
 
 class ChessChronos : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Navigator(screen = NewGameScreen()) { nav ->
-                SlideTransition(navigator = nav)
+            KoinContext {
+                Navigator(screen = NewGameScreen()) { nav ->
+                    SlideTransition(navigator = nav)
+                }
             }
         }
     }
